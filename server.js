a3a3b07d32c8a40fa5e5bb7258dc9e8ae962c566
JacobReeder <jacob.reeder1@gmail.com>
2022-04-13 17:36:17 -0700

    added db.query

--- server.js
+++ server.js
@@ -20,7 +20,37 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
   );
 
+//db.query(`SELECT * FROM candidates`, (err, rows) => { //GET candidates
+//  console.log(rows);
+//});
 
+//GET single candidate
+//db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+ // if (err) {
+ //   console.log(err);
+//}
+//console.log(row);
+//});
+
+//DELETE candidate
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+ // if (err) {
+ //   console.log(err);
+//  }
+//  console.log(result);
+//});
+
+//Create a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+ //             VALUES (?,?,?,?)`;
+ // const params = [1, 'Ronald', 'Firbank', 1];
+
+ // db.query(sql, params, (err, result) => {
+  //  if(err) {
+  //    console.log(err);
+  //  }
+  //  console.log(result);
+  //});
 
 
 ///catchall --must be placed last
