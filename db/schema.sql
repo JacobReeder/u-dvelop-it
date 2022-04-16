acadef46606f9b3fd24dcba8810e3f1501bd4a22
JacobReeder <jacob.reeder1@gmail.com>
2022-04-15 17:10:12 -0700

    modularized API routes//Issue 3 closed//functions

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,12 @@
-CREATE TABLE parties (
+ DROP TABLE IF EXISTS voters;
+  DROP TABLE IF EXISTS candidates;
+
+CREATE TABLE voters (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE candidates (
@@ -12,3 +17,4 @@ CREATE TABLE candidates (
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
 );
+
